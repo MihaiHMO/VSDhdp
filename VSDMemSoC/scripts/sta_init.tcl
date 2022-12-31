8cba3192cf423f476e2afecfebe35fd5e98e05cd
Mihai Hurdugaciu <49897923+MihaiHMO@users.noreply.github.com>
2022-12-31 16:49:03 +0200

    Update sta_init.tcl

--- VSDMemSoC/scripts/sta_init.tcl
+++ VSDMemSoC/scripts/sta_init.tcl
@@ -5,41 +5,40 @@ link_design vsdmemsoc
 set_units -time ns
 create_clock [get_ports -of_objects [get_nets CLK]] -name clk -period 14.5
 
-set_input_delay -clock clk -max -rise 0.5 {reset init_en init_addr init_data}
-set_input_delay -clock clk -min -rise 1 {reset init_en init_addr init_data}
-set_input_delay -clock clk -min -fall 0.5 {reset init_en init_addr init_data}
-set_input_delay -clock clk -max -fall 1 {reset init_en init_addr init_data}
+#set_input_delay -clock clk -max -rise 0.5 {reset init_en init_addr init_data}
+#set_input_delay -clock clk -min -rise 1 {reset init_en init_addr init_data}
+#set_input_delay -clock clk -min -fall 0.5 {reset init_en init_addr init_data}
+#set_input_delay -clock clk -max -fall 1 {reset init_en init_addr init_data}
 #[get_ports -of_objects [get_nets init_en]]
 
-set_input_transition -min -rise 0.5 [all_inputs]
-set_input_transition -max -rise 1 [all_inputs]
-set_input_transition -min -fall 0.5 [all_inputs]
-set_input_transition -max -fall 1 [all_inputs]
+#set_input_transition -min -rise 0.5 [all_inputs]
+#set_input_transition -max -rise 1 [all_inputs]
+#set_input_transition -min -fall 0.5 [all_inputs]
+#set_input_transition -max -fall 1 [all_inputs]
 
-set_output_delay -clock clk -min -rise 0.5 [all_outputs]
-set_output_delay -clock clk -max -rise 1 [all_outputs]
-set_output_delay -clock clk -min -fall 0.5 [all_outputs]
-set_output_delay -clock clk -max -fall 1 [all_outputs]
-set_load 1 [all_outputs] 
+#set_output_delay -clock clk -min -rise 0.5 [all_outputs]
+#set_output_delay -clock clk -max -rise 1 [all_outputs]
+#set_output_delay -clock clk -min -fall 0.5 [all_outputs]
+#set_output_delay -clock clk -max -fall 1 [all_outputs]
+#set_load 1 [all_outputs] 
 
 puts "----------reg2reg setup/hold---------------\n"
 
-report_checks -path_delay max -from [all_registers] -to [all_registers] -fields {nets cap slew input_pins} 
-report_checks -path_delay min -from [all_registers] -to [all_registers] -fields {nets cap slew input_pins}
+#report_checks -path_delay max -from [all_registers] -to [all_registers] -fields {nets cap slew input_pins} 
+report_checks -path_delay min -group_count 1000 -fields {nets cap slew input_pins}
 
-puts "----------in2reg setup/hold ---------------\n"
+#puts "----------in2reg setup/hold ---------------\n"
 
-report_checks -path_delay max -from [all_inputs] -to [all_registers] -fields {nets cap slew input_pins}
-report_checks -path_delay min -from [all_inputs] -to [all_registers] -fields {nets cap slew input_pins}
+#report_checks -path_delay max -from [all_inputs] -to [all_registers] -fields {nets cap slew input_pins}
+#report_checks -path_delay min -from [all_inputs] -to [all_registers] -fields {nets cap slew input_pins}
 
-puts "----------reg2out setup/hold ---------------\n"
+#puts "----------reg2out setup/hold ---------------\n"
 
-report_checks -path_delay max -from [all_registers] -to [all_outputs] -fields {nets cap slew input_pins}
-report_checks -path_delay min -from [all_registers] -to [all_outputs] -fields {nets cap slew input_pins}
+#report_checks -path_delay max -from [all_registers] -to [all_outputs] -fields {nets cap slew input_pins}
+#report_checks -path_delay min -from [all_registers] -to [all_outputs] -fields {nets cap slew input_pins}
 
-puts "----------in2out setup/hold ---------------\n"
+#puts "----------in2out setup/hold ---------------\n"
 
-report_checks -path_delay max -from [all_inputs] -to [all_outputs] -fields {nets cap slew input_pins}
-report_checks -path_delay min -from [all_inputs] -to [all_outputs] -fields {nets cap slew input_pins}
-
+#report_checks -path_delay max -from [all_inputs] -to [all_outputs] -fields {nets cap slew input_pins}
+#report_checks -path_delay min -from [all_inputs] -to [all_outputs] -fields {nets cap slew input_pins}
 
