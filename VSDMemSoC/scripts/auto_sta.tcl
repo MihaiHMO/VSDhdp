7afaf9628524630c8f88edc7f859d506abcad072
Mihai Hurdugaciu <49897923+MihaiHMO@users.noreply.github.com>
2023-01-15 18:41:07 +0200

    Update auto_sta.tcl

--- VSDMemSoC/scripts/auto_sta.tcl
+++ VSDMemSoC/scripts/auto_sta.tcl
@@ -1,13 +1,14 @@
 #!/usr/bin/tclsh
 
-set out_dir "../output/sta"
-set lib_dir [lsort [glob ../lib/*sky130_fd**.lib]]
-set verilog_file [glob ../output/synth/*.v]
-read_liberty ../lib/sram_32_256_sky130A_TT_1p8V_25C.lib
+set out_dir "output/sta"
+set lib_dir [lsort [glob lib/*sky130_fd**.lib]]
+set verilog_file [glob output/synth/*.v]
+read_liberty lib/sram_32_256_sky130A_TT_1p8V_25C.lib
 set synth_file_path [lindex $verilog_file 0]
 set filemane_synth1 [string map {"vsdmemsoc" "Post"} [lindex [split [file rootname [file tail $synth_file_path]] .] 0]]
 set filemane_synth2 [string totitle [lindex [split [file rootname [file tail $synth_file_path]] .] 1]]
 file mkdir $out_dir/$filemane_synth1\_$filemane_synth2
+file mkdir $out_dir/$filemane_synth1\_$filemane_synth2/graph
 
 foreach libs $lib_dir {
 
@@ -19,14 +20,18 @@ foreach libs $lib_dir {
 		link_design vsdmemsoc
 
 		# Design constrains
-		set_units -time ns
-		create_clock [get_ports -of_objects [get_nets CLK]] -name clk -period 14.5
+		read_sdc -echo scripts/vsdmemsoc_synth.sdc
 
 		#checks commnad -synthesis 
+		#Worst design parameters
 		report_checks -path_delay min_max -fields {nets cap slew input_pins} -digits {4} > $out_dir/$filemane_synth1\_$filemane_synth2/$lib_filename\_mm.rpt
-		report_tns -digits {4} >  $out_dir/$filemane_synth1\_$filemane_synth2/$lib_filename\_tns.rpt
-		report_wns -digits {4} > $out_dir/$filemane_synth1\_$filemane_synth2/$lib_filename\_wns.rpt
-		puts "$lib_filename"
+		#Report for certain gates
+		report_checks -path_delay min_max -from {_20588_ _21814_} -to {_21686_ _21110_} -fields {nets cap slew input_pins} -digits {4} > $out_dir/$filemane_synth1\_$filemane_synth2/graph/$lib_filename\_g_mm.rpt
+		#TNS
+		report_tns -digits {4} > $out_dir/$filemane_synth1\_$filemane_synth2/$lib_filename\_tns.rpt
+		#report min max for mem inetrface
+		report_checks -path_delay min_max -to mem -fields {nets cap slew input_pins} -digits {4} > $out_dir/$filemane_synth1\_$filemane_synth2/$lib_filename\_mem.rpt
+		
 	}
 }
 exit
