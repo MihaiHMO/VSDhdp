675425e05345752ee3c95cf1d4d7d6d696a2cc19
Mihai Hurdugaciu <49897923+MihaiHMO@users.noreply.github.com>
2023-02-15 20:11:06 +0200

    Update vsdmemsoc_wbs.v

--- Spice/vsdmemsoc_wbs.v
+++ Spice/vsdmemsoc_wbs.v
@@ -1,4 +1,3 @@
-
 module vsdmemsoc (
 `ifdef USE_POWER_PINS
     inout vccd1,	// User area 1 1.8V supply
@@ -6,7 +5,7 @@ module vsdmemsoc (
 `endif
     output [9:0] OUT,
     input ext_clk,
-    input ext_reset,
+    input ext_rst,
     //input init_en,          internal (mem_wr)
     //input [7:0] init_addr,   replaced by wbs_adr_i
     //input [31:0] init_data,  replaced by wbs_dat_i
@@ -17,22 +16,22 @@ module vsdmemsoc (
     input wbs_we_i,
     input [3:0] wbs_sel_i,
     input [31:0] wbs_dat_i,
-    input [31:0] wbs_adr_i,
+    input [7:0] wbs_adr_i,
     output reg wbs_ack_o,
-    output reg[31:0] wbs_dat_o,
-	
+    output reg[31:0] wbs_dat_o	
 );
+    wire       in_rst;
     wire        sel_source;  // signal active when wisbone controlls the sram 
     wire        mem_wr;      //   write_enable signal for sram  
     wire [7:0]  mem_addr;    //   address bus for sram
     wire [31:0] imem_data;   //   sram dout data connection to cpu (instruction mem)
     wire [7:0]  imem_addr;   //   sram address connection from cpu (instruction mem)
     wire mem_clk;            //   sram clk (from system or wisbon)
-	assign sel_source = wbs_cyc_i && wbs_stb_i &&  wb_rst_i && !wbs_ack_o;
-	assign mem_clk = sel_source ? ext_clk : wb_clk_i  
-    assign mem_wr   = sel_source ? wbs_we_i : 1'b1;  
-    assign mem_addr = sel_source ? wbs_adr_i : imem_addr;
-
+    reg [31:0] mem_din ;
+    assign in_rst = ext_rst ? 1'b1 : !wb_rst_i ;
+    assign mem_clk = wb_rst_i ? ext_clk : wb_clk_i  ;
+    assign mem_wr   = !wb_rst_i ? !wbs_we_i : 1'b1;  
+    assign mem_addr = !wb_rst_i ? wbs_adr_i : imem_addr;
 
 
    always @(posedge wb_clk_i or posedge wb_rst_i) begin
@@ -41,30 +40,34 @@ module vsdmemsoc (
             wbs_dat_o   <=0;
             wbs_ack_o   <=0;
 
-        end else if (sel_source && wbs_we_i )
+        end else if (wbs_cyc_i && wbs_stb_i && !wbs_ack_o && wbs_we_i )
 		begin // write
               // write to sram
-            init_data <=  wbs_sel_i[0]?  wbs_dat_i : imem_data;
+            mem_din[31:0]  <= wbs_sel_i[0] ? wbs_dat_i[31:0] : mem_din[31:0];
+            //mem_din[15:8] <= wbs_sel_i[1] ? wbs_dat_i[15:8] : mem_din[15:8];
+            //mem_din[23:16] <= wbs_sel_i[2] ? wbs_dat_i[23:16] : mem_din[23:16];
+            //mem_din[31:24] <= wbs_sel_i[3] ? wbs_dat_i[31:24] : mem_din[31:24];
             wbs_ack_o <= 1;
-        end else if (sel_source && !wbs_we_i) begin // read 
-            wbs_dat_o <= imem_data
+        end else if (wbs_cyc_i && wbs_stb_i && !wbs_ack_o && !wbs_we_i) begin // read 
+            wbs_dat_o <= imem_data;
             wbs_ack_o <= 1;
         end else begin 
             wbs_ack_o <= 0;
-            wbs_dat_o <= 0;
+            wbs_dat_o <= 32'b0;
         end
     end
    
  
     rvmyth core (
         .OUT(OUT),
-        .CLK(CLK),
-        .reset(reset),
+        .CLK(ext_clk),
+        .reset(in_rst),
 
         .imem_addr(imem_addr),
         .imem_data(imem_data)
     );
-	   sky130_sram_1kbyte_1rw1r_32x256_8 mem (
+    
+    sky130_sram_1kbyte_1rw1r_32x256_8 mem (
 `ifdef USE_POWER_PINS
         .vccd1(vccd1),	// User area 1 1.8V supply
         .vssd1(vssd1),	// User area 1 digital ground
@@ -75,7 +78,7 @@ module vsdmemsoc (
         .web0(mem_wr),
         .wmask0(4'b1111),
         .addr0(mem_addr),
-        .din0(init_data),
+        .din0(mem_din),
         .dout0(imem_data),
         
         // Port 1: R
